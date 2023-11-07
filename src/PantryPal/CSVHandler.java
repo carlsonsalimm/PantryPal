389b8c892c6501cdecf397e33c11d0a39daeb630
potato48 <kevin.liu909@gmail.com>
2023-11-06 17:27:37 -0800

    merged with branch 'csv_handler'

---
+++ src/PantryPal/CSVHandler.java
@@ -0,0 +1,18 @@
+package src.PantryPal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVHandler {
+    String fileName;
+
+    public List<Recipe> readRecipes() {
+
+        List<Recipe> recipes = new ArrayList<Recipe>();
+        return recipes;
+    }
+
+    public void writeRecipes(Recipe recipe) {
+
+    }
+}
---
+++ src/PantryPal/Recipe.java
@@ -0,0 +1,19 @@
+package src.PantryPal;
+
+public class Recipe {
+    String title;
+    String instructions;
+
+    public Recipe(String title, String instructions) {
+        this.title = title;
+        this.instructions = instructions;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getInstructions() {
+        return this.instructions;
+    }
+}
