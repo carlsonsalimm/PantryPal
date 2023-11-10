72d2f6cd7d34434e1fd36ddd2572eb0e1a56bd55
Acezz4120 <arandersen@ucsd.edu>
2023-11-09 17:37:25 -0800

    Update cancel button
    additional cancel button in the specify meal type which return to the recipe list

--- src/main/java/project/SpecifyMealTypePage.java
+++ src/main/java/project/SpecifyMealTypePage.java
@@ -18,8 +18,18 @@ public class SpecifyMealTypePage extends BorderPane {
     private AudioRecording recordButton;
 
     SpecifyMealTypePage() {
+        //button to record audio
         recordButton = new AudioRecording();
-
+        //button place at center of page
         this.setCenter(recordButton);
+
+        //button to cancel recording and return to recipe list page
+        Button cancelButton = new Button("Cancel");
+        cancelButton.setOnAction(event -> Main.setPage(Main.temp));
+        //button place at bottom center of page
+        this.setBottom(cancelButton);
+        this.setAlignment(cancelButton, Pos.CENTER);
+
+        
     }
 }
