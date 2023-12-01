4434f9205a473587c4c89bfc2e1b8ae3c81c7829
brendan887 <mahbrendan@gmail.com>
2023-12-01 01:52:19 -0800

    Removed RememberMe and updated gitignore

--- src/main/java/project/MockLoginPageController.java
+++ src/main/java/project/MockLoginPageController.java
@@ -1,12 +1,6 @@
 package project;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.net.CookieHandler;
-import org.json.Cookie;
 
 import javafx.event.ActionEvent;
 
