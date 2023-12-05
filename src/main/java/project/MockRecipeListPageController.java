cb789f6c7b6b98501e4b5565cb649499b3b02adc
Borna <Borna1103@msn.com>
2023-12-05 15:44:10 -0800

    mock

--- src/main/java/project/MockRecipeListPageController.java
+++ src/main/java/project/MockRecipeListPageController.java
@@ -2,6 +2,7 @@ package project;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -14,84 +15,37 @@ import javafx.scene.layout.StackPane;
 
 public class MockRecipeListPageController implements Controller {
      // actions to account for: addButton, RecipeItem, detailedViewButton
-    private RecipeListPage view;
     private Model model;
+    public List<Recipe> recipes;
+    public String sort;
+    public String type;
 
     public MockRecipeListPageController(Model model) {
-        this.view = view;
         this.model = model;
 
-        this.view.setAddButtonAction(event -> {
-            try {
-                handleAddButton(event);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-
-        this.view.setDetailedViewButtonsAction(event -> {
-            try {
-                handleDetailedViewButton(event);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-
-        this.view.setSignOutButtonAction(event -> {
-            try {
-                handleSignOutButton(event);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-
-        this.view.setSortBoxAction(event -> {
-            try {
-                handleSortBoxButton(event);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-
-        this.view.setFilterBoxAction(event -> {
-            try {
-                handleFilterBoxButton(event);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
     }
 
-    public void handleAddButton(ActionEvent event) throws IOException {
-        SpecifyMealTypePage temp = new SpecifyMealTypePage();
-        Main.setPage(temp);
-        Main.setController(new SpecifyMealTypePageController(temp, model));
+    public void setMealType(String type){
+        this.type = type;
+    }
+    public void setSort(String sort){
+        this.sort = sort;
+    }
+    public void setRecipes(ArrayList<Recipe> recipes){
+        this.recipes = recipes;
+    }
+    public boolean handleAddButton(ActionEvent event) throws IOException {
+        return true;
     }
 
-    public void handleDetailedViewButton(ActionEvent event) throws IOException {
-        Object obj = event.getSource();
-        Parent par = ((Button) (obj)).getParent();
-        System.out.println(par);
-        Parent par2 = ((StackPane) (par)).getParent();
-        System.out.println(par2);
-        Parent par3 = ((BorderPane) (par2)).getParent();
-        System.out.println(par3);
-        System.out.println(((RecipeItem) par3).getRecipe().getTitle());
-
-        Recipe recipe = ((RecipeItem) par3).getRecipe();
-        DetailedRecipePage temp = new DetailedRecipePage(recipe, false);
-        Main.setPage(temp);
-        Main.setController(new DetailedRecipePageController(temp, model));
+    public boolean handleDetailedViewButton(ActionEvent event) throws IOException {
+        return true;
     }
 
-    public void handleSortBoxButton(ActionEvent event) throws IOException {
-        List<Recipe> tempRecipes = view.recipes;
-        String selectedItem = view.getSortBox().getSelectionModel().getSelectedItem();
+    public List<Recipe> handleSortBoxButton(ActionEvent event) throws IOException {
+        List<Recipe> tempRecipes = recipes;
+        String selectedItem = sort;
         // clear list
-        for (int i = 0; i < view.vbox.getChildren().size(); i++) {
-            view.vbox.getChildren().removeIf(recipeItem -> recipeItem instanceof RecipeItem);
-        }
-        view.detailedViewButtons.clear();
         // compare, then determine sort method
         if (selectedItem == "A-Z") { // sort A-Z logic
             Collections.sort(tempRecipes, Comparator.comparing(Recipe::getTitle));
@@ -104,100 +58,43 @@ public class MockRecipeListPageController implements Controller {
             Collections.sort(tempRecipes, Comparator.comparingLong(Recipe -> Long.parseLong(Recipe.getCreationTime())));
         }
         // re-populate list)
-        for (Recipe x : tempRecipes) {
-            RecipeItem Item = new RecipeItem();
-            Item.setRecipe(x);
-            view.vbox.getChildren().add(Item);
-            view.detailedViewButtons.add(Item.getDetailedViewButton());
-            this.view.setDetailedViewButtonsAction(event1 -> {
-                try {
-                    handleDetailedViewButton(event1);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
-        }
+        return tempRecipes;
     }
 
-    public void handleFilterBoxButton(ActionEvent event) throws IOException {
-        List<Recipe> tempRecipes = view.recipes;
-        String selectedItem = view.getFilterBox().getSelectionModel().getSelectedItem();
+    public List<Recipe> handleFilterBoxButton(ActionEvent event) throws IOException {
+        List<Recipe> tempRecipes = recipes;
+        String selectedItem = type;
+        List<Recipe> filter = new ArrayList<>();
         // clear list
-        for (int i = 0; i < view.vbox.getChildren().size(); i++) {
-            view.vbox.getChildren().removeIf(recipeItem -> recipeItem instanceof RecipeItem);
-        }
-        if (selectedItem == "Breakfast") {
+        if (selectedItem.equals("Breakfast")) {
             for (Recipe x : tempRecipes) {
                 if (x.getMealType() == "breakfast") {
-                    RecipeItem Item = new RecipeItem();
-                    Item.setRecipe(x);
-                    view.vbox.getChildren().add(Item);
-                    view.setDetailedViewButtonsAction(event1 -> {
-                        try {
-                            handleDetailedViewButton(event);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    });
+                    filter.add(x);
                 }
             }
-        } else if (selectedItem == "Lunch") {
+        } else if (selectedItem.equals("Lunch")) {
             for (Recipe x : tempRecipes) {
                 if (x.getMealType() == "lunch") {
-                    RecipeItem Item = new RecipeItem();
-                    Item.setRecipe(x);
-                    view.vbox.getChildren().add(Item);
-                    view.setDetailedViewButtonsAction(event1 -> {
-                        try {
-                            handleDetailedViewButton(event);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    });
+                    filter.add(x);
                 }
             }
-        } else if (selectedItem == "Dinner") {
+        } else if (selectedItem.equals("Dinner")) {
             for (Recipe x : tempRecipes) {
                 if (x.getMealType() == "dinner") {
-                    RecipeItem Item = new RecipeItem();
-                    Item.setRecipe(x);
-                    view.vbox.getChildren().add(Item);
-                    view.setDetailedViewButtonsAction(event1 -> {
-                        try {
-                            handleDetailedViewButton(event);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    });
+                    filter.add(x);
                 }
             }
-        } else {
-            for (Recipe x : tempRecipes) {
-                RecipeItem Item = new RecipeItem();
-                Item.setRecipe(x);
-                view.vbox.getChildren().add(Item);
-                view.setDetailedViewButtonsAction(event1 -> {
-                    try {
-                        handleDetailedViewButton(event);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                });
-            }
-        }
+        } 
+        return filter;
     }
 
-    public void handleSignOutButton(ActionEvent event) throws IOException {
-        LoginPage loginPage = new LoginPage();
-        LoginPageController controller = new LoginPageController(loginPage, new Model());
+    public boolean handleSignOutButton(ActionEvent event) throws IOException {
 
         // Reset Rememvber Me Logic
         FileWriter file = new FileWriter("RememberMe.csv", false);
         file.write("0");
         file.close();
 
-        // Set Page
-        Main.setPage(loginPage);
-        Main.setController(controller);
+        return true;
     }
 }
